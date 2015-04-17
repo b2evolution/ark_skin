c9a24100e792a7ba4455889f1d8f7c70756fa264
odesker <plus-invest@yandex.ru>
2015-04-17 17:59:56 +0500

    many changes

--- index.main.php
+++ index.main.php
@@ -31,88 +31,32 @@ skin_include( '_html_header.inc.php', array(
 	'html_tag' => '<!DOCTYPE html>'."\r\n"
 	             .'<html lang="'.locale_lang( false ).'">',
 ) );
-// Note: You can customize the default HTML header by copying the generic
-// /skins/_html_header.inc.php file into the current skin folder.
-// -------------------------------- END OF HEADER --------------------------------
-?>
 
-<div class='headpicture'>
-	<img src='/media/shared/global/sunset/sunset.jpg'>
-</div>
-
-<div class="container">
-	<?php
-	
 // ---------------------------- SITE HEADER INCLUDED HERE ----------------------------
 // If site headers are enabled, they will be included here:
 siteskin_include( '_site_body_header.inc.php' );
 // ------------------------------- END OF SITE HEADER --------------------------------
+// Note: You can customize the default HTML header by copying the generic
+// /skins/_html_header.inc.php file into the current skin folder.
+// -------------------------------- END OF HEADER --------------------------------
 
-	?>
-	<div class="row">
-		<div class="coll-xs-12 coll-sm-12 col-md-4 col-md-push-8">
-			<div class="PageTop">
-	<?php
-		// ------------------------- "Page Top" CONTAINER EMBEDDED HERE --------------------------
-		// Display container and contents:
-		skin_container( NT_('Page Top'), array(
-				// The following params will be used as defaults for widgets included in this container:
-				'block_start'         => '<div class="widget $wi_class$">',
-				'block_end'           => '</div>',
-				'block_display_title' => false,
-				'list_start'          => '<ul>',
-				'list_end'            => '</ul>',
-				'item_start'          => '<li>',
-				'item_end'            => '</li>',
-			) );
-		// ----------------------------- END OF "Page Top" CONTAINER -----------------------------
-	?>
-			</div>
-		</div>
-		<div class="coll-xs-12 col-sm-12 col-md-8 col-md-pull-4">
-			<div class="pageHeader">
-	<?php
-		// ------------------------- "Header" CONTAINER EMBEDDED HERE --------------------------
-		// Display container and contents:
-		skin_container( NT_('Header'), array(
-				// The following params will be used as defaults for widgets included in this container:
-				'block_start'       => '<div class="widget $wi_class$">',
-				'block_end'         => '</div>',
-				'block_title_start' => '<h1>',
-				'block_title_end'   => '</h1>',
-			) );
-		// ----------------------------- END OF "Header" CONTAINER -----------------------------
-	?>
-			</div>
-		</div>
-	</div>
+$is_pictured_page = in_array( $disp, array( 'front', 'login', 'register', 'lostpassword', 'activateinfo', 'access_denied' ) );
 
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="nav nav-tabs">
-	<?php
-		// ------------------------- "Menu" CONTAINER EMBEDDED HERE --------------------------
-		// Display container and contents:
-		// Note: this container is designed to be a single <ul> list
-		skin_container( NT_('Menu'), array(
-				// The following params will be used as defaults for widgets included in this container:
-				'block_start'         => '',
-				'block_end'           => '',
-				'block_display_title' => false,
-				'list_start'          => '',
-				'list_end'            => '',
-				'item_start'          => '<li>',
-				'item_end'            => '</li>',
-				'item_selected_start' => '<li class="active">',
-				'item_selected_end'   => '</li>',
-				'item_title_before'   => '',
-				'item_title_after'    => '',
-			) );
-		// ----------------------------- END OF "Menu" CONTAINER -----------------------------
-	?>
-			</ul>
-		</div>
-	</div>
+if( $is_pictured_page )
+{ // Display a picture from skin setting as background image
+	global $media_path, $media_url;
+	$bg_image = $Skin->get_setting( 'front_bg_image' );
+	echo '<div class="headpicture">';
+	if( ! empty( $bg_image ) && file_exists( $media_path.$bg_image ) )
+	{ // If it exists in media folder
+		echo '<img src="'.$media_url.$bg_image.'" />';
+	}
+	echo '</div>';
+}
+
+?>
+
+<div class="container">
 
 <!-- =================================== START OF MAIN AREA =================================== -->
 	<div class="row">
@@ -250,101 +194,19 @@ siteskin_include( '_site_body_header.inc.php' );
 			) );
 		// ------------------------- END OF PREV/NEXT PAGE LINKS -------------------------
 	}
-	?>
 
-
-	<?php
-		// -------------- MAIN CONTENT TEMPLATE INCLUDED HERE (Based on $disp) --------------
-		skin_include( '$disp$', array(
-				'disp_posts'  => '',		// We already handled this case above
-				'disp_single' => '',		// We already handled this case above
-				'disp_page'   => '',		// We already handled this case above
-				'skin_form_params' => $Skin->get_template( 'Form' ),
-				'author_link_text' => 'preferredname',
-				'profile_tabs' => array(
-					'block_start'         => '<ul class="nav nav-tabs profile_tabs">',
-					'item_start'          => '<li>',
-					'item_end'            => '</li>',
-					'item_selected_start' => '<li class="active">',
-					'item_selected_end'   => '</li>',
-					'block_end'           => '</ul>',
-				),
-				'pagination' => array(
-					'block_start'           => '<div class="center"><ul class="pagination">',
-					'block_end'             => '</ul></div>',
-					'page_current_template' => '<span><b>$page_num$</b></span>',
-					'page_item_before'      => '<li>',
-					'page_item_after'       => '</li>',
-					'prev_text'             => '&lt;&lt;',
-					'next_text'             => '&gt;&gt;',
-				),
-				// Form params for the forms below: login, register, lostpassword, activateinfo and msgform
-				'skin_form_before'      => '<div class="panel panel-default skin-form">'
-																			.'<div class="panel-heading">'
-																				.'<h3 class="panel-title">$form_title$</h3>'
-																			.'</div>'
-																			.'<div class="panel-body">',
-				'skin_form_after'       => '</div></div>',
-				// Login
-				'display_form_messages' => true,
-				'form_title_login'      => T_('Log in to your account').'$form_links$',
-				'form_class_login'      => 'wrap-form-login',
-				'form_title_lostpass'   => get_request_title().'$form_links$',
-				'form_class_lostpass'   => 'wrap-form-lostpass',
-				'login_form_inskin'     => false,
-				'login_page_before'     => '<div class="$form_class$">',
-				'login_page_after'      => '</div>',
-				'login_form_class'      => 'form-login',
-				'display_reg_link'      => true,
-				'abort_link_position'   => 'form_title',
-				'abort_link_text'       => '<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
-				// Register
-				'register_page_before'      => '<div class="wrap-form-register">',
-				'register_page_after'       => '</div>',
-				'register_form_title'       => T_('Register'),
-				'register_form_class'       => 'form-register',
-				'register_links_attrs'      => '',
-				'register_use_placeholders' => true,
-				'register_field_width'      => 252,
-				'register_disabled_page_before' => '<div class="wrap-form-register register-disabled">',
-				'register_disabled_page_after'  => '</div>',
-				// Activate form
-				'activate_form_title'  => T_('Account activation'),
-				'activate_page_before' => '<div class="wrap-form-activation">',
-				'activate_page_after'  => '</div>',
-				// Profile
-				'profile_avatar_before' => '<div class="panel panel-default profile_avatar">',
-				'profile_avatar_after'  => '</div>',
-				// Search
-				'search_input_before'  => '<div class="input-group">',
-				'search_input_after'   => '',
-				'search_submit_before' => '<span class="input-group-btn">',
-				'search_submit_after'  => '</span></div>',
-				// Comment template
-				'comment_avatar_position' => 'before_text',
-				'comment_start'         => '<div class="panel panel-default">',
-				'comment_end'           => '</div>',
-				'comment_post_before'   => '<div class="panel-heading"><h4 class="bTitle floatleft">',
-				'comment_post_after'    => '</h4>',
-				'comment_title_before'  => '<div class="floatright">',
-				'comment_title_after'   => '</div><div class="clear"></div></div><div class="panel-body">',
-				'comment_rating_before' => '<div class="comment_rating floatright">',
-				'comment_rating_after'  => '</div>',
-				'comment_text_before'   => '',
-				'comment_text_after'    => '',
-				'comment_info_before'   => '<div class="bCommentSmallPrint">',
-				'comment_info_after'    => '</div></div>',
-				'preview_start'         => '<div class="panel panel-warning" id="comment_preview">',
-				'preview_end'           => '</div>',
-				// Front page
-				'featured_intro_before' => '<div class="jumbotron">',
-				'featured_intro_after'  => '</div>',
-				// Form "Sending a message"
-				'msgform_form_title' => T_('Sending a message'),
-			) );
-		// Note: you can customize any of the sub templates included here by
-		// copying the matching php file into your skin directory.
-		// ------------------------- END OF MAIN CONTENT TEMPLATE ---------------------------
+		echo '<div id="styled_content_block">'; // Beginning of posts display
+		while( $Item = & mainlist_get_item() )
+		{	// For each blog post:
+			// ---------------------- ITEM BLOCK INCLUDED HERE ------------------------
+			skin_include( '_item_block.inc.php', array(
+					'content_mode' => 'auto',		// 'auto' will auto select depending on $disp-detail
+					'image_size'	 =>	'fit-400x320',
+				) );
+			// ----------------------------END ITEM BLOCK  ----------------------------
+		}
+		echo '</div>'; // End of posts display
+		
 	?>
 	</div>
 
